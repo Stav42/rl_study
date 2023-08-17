6571cad5c6d5769ab653bc1346fefb2743cc67cf
Stav42 <vaditya@iitk.ac.in>
2023-08-17 21:25:12 +0530

    saving changes

--- gym_test.py
+++ gym_test.py
@@ -6,8 +6,8 @@ import torch.nn as nn
 import torch.optim as optim
 from torch.distributions.normal import Normal
 
-timesteps = 500
-save_model = 0
+timesteps = 100
+save_model = 1
 
 class Policy(nn.Module):
 
@@ -50,8 +50,8 @@ value_nn = Value()
 
 rate_learning = 1e-3
 
-existing = 1
-render = 1
+existing = 0
+render = 0
 
 if existing:
     policy_nn.load_state_dict(torch.load('policy_nn.pth'))
@@ -76,7 +76,8 @@ if render:
     env.render()
 # env.render()
 viewer = None
-viewer = env.env.env.env.mujoco_renderer.viewer
+if render:
+    viewer = env.env.env.env.mujoco_renderer.viewer
 
 line_start = np.array([0, 0, 0])
 line_end = np.array([10, 0, 0])
@@ -85,7 +86,7 @@ loss1 = nn.MSELoss()
 N_traj = 10
 
 # K iterations of learning
-for k in range(1000):
+for k in range(100):
     # 100 trajectories
 
     print("iteration no: ", k)
@@ -104,7 +105,7 @@ for k in range(1000):
     actions = np.zeros([N_traj, timesteps, 8])
     advantages = np.zeros([N_traj, timesteps])
     values = np.zeros([N_traj, timesteps])
-    actions_log = np.zeros([N_traj, timesteps])
+    actions_log = torch.zeros([N_traj, timesteps], requires_grad=False)
 
     accumulated_gradients = [torch.zeros_like(param) for param in policy_nn.parameters()]
 
@@ -125,8 +126,8 @@ for k in range(1000):
             action_mean, action_std = policy_nn(torch.from_numpy(observation).float())
             action_dist = Normal(action_mean, action_std)
             action = action_dist.sample()
-            action = torch.tanh(action)
             action_log = action_dist.log_prob(action)
+            action = torch.tanh(action)
             # action = action.detach().numpy()
             value_i = value_nn(torch.from_numpy(observation).float())
             value_i = value_i.detach().numpy()
@@ -142,7 +143,7 @@ for k in range(1000):
             rewards[traj, t] =  reward
             advantages[traj, t] = advantage
             values[traj, t] = value_i
-            actions_log[traj, t] = action_log.sum()
+            actions_log[traj, t] = action_log.sum().item()
             
             if viewer is not None:
                 viewer.add_marker(pos=line_start, size=0.2, rgba=(255, 0, 0, 1))
@@ -154,6 +155,7 @@ for k in range(1000):
 
 
         for i, reward in enumerate(rewards):
+            reward = np.flip(reward)
             return_i = np.cumsum(reward)
             returns[i] = np.flip(return_i)
 
@@ -175,32 +177,52 @@ for k in range(1000):
     loss.backward()
     optim_val.step()
 
-    ## Now improving the policy:
+    # Initialize total policy loss
+    total_policy_loss = torch.tensor(0.0, dtype=torch.float32)
 
     for traj in range(N_traj):
         for t in range(timesteps):
-
-            loss_t = -(actions_log[traj, t])*returns[traj, t]
-
-            loss_t = torch.tensor(loss_t, dtype=torch.float32, requires_grad=True)
-            loss_t.backward(retain_graph=True)
-
-            for param, acc_grad in zip(policy_nn.parameters(), accumulated_gradients):
-                if param.grad is not None:
-                    acc_grad += param.grad
+            # loss_t = -(actions_log[traj, t])*(returns[traj, t] - values[traj, t])
+            loss_t = -actions_log_tensor[traj, t] * (returns[traj, t] - values[traj, t])
+            # Convert loss_t to a tensor with requires_grad=True
+            total_policy_loss += loss_t
             
-            optim_pol.zero_grad()
 
+    optim_pol.zero_grad()
+    total_policy_loss.backward()
 
-    for acc_grad in accumulated_gradients:
-        acc_grad /= (N_traj * timesteps)
+    # Print gradients for debugging
+    for name, param in policy_nn.named_parameters():
+        if param.grad is not None:
+            print(name, "gradient:", param.grad)
 
-    for param, acc_grad in zip(policy_nn.parameters(), accumulated_gradients):
-        param.data -= rate_learning * acc_grad
+    optim_pol.step()
+
+    print("Policy Loss:", total_policy_loss.item())
+    for name, param in policy_nn.named_parameters():
+        print(name, param.data)
+
+    # for acc_grad in accumulated_gradients:
+    #     acc_grad /= (N_traj)
+
+    # for param, acc_grad in zip(policy_nn.parameters(), accumulated_gradients):
+    #     # print("Gradient is: ", acc_grad)
+    #     param.data -= rate_learning * acc_grad
+
+    # print("Policy Parameters are: \n")
+
+    # for name, param in policy_nn.named_parameters():
+    #     print(name, param.data)
+
+    # print("Value Parameters are: \n")
+
+    # for name, param in value_nn.named_parameters():
+    #     print(name, param.data)
+
 
 if save_model:
     torch.save(policy_nn.state_dict(), 'policy_nn.pth')
     torch.save(value_nn.state_dict(), 'value_nn.pth')
-    
+  
 
 env.close() 
